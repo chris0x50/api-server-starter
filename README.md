ed4c2af7ab60507bfafd3fd33601f914e8d02f6e
Chris P <chrisp@finnovation.io>
2017-06-14 12:42:33 -0400

    Added root route & updated readme

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Express API Server
+## Commands
+#npm run prod
+#npm run dev
+#npm test
 ## Branches
 ### master
 Server with barebones configuration
 ### database
 Server with Sequelize & Postgres setup
 ### auth
-Server with authentication
-
+Server with database & authentication
+### complete
+Server with database, authentication, & tests
--- app.js
+++ app.js
@@ -6,15 +6,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Routes
+const index = require('./server/routes/root');
+
 // Configure Middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Configure Routes
+app.use('/api/', index);
 
 // Start Server
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
 debug(`Server listening on: ${port}`);
-
+debug(`Open your web broswer to http://localhost:${port}/api`);
 module.exports = app;
